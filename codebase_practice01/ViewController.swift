c2f64f99d7716b4a341572731d1154654d5feee7
yonmoyonmo <yonmosound@gmail.com>
2022-01-18 17:23:14 +0900

    Initial Commit

---
+++ codebase_practice01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codebase_practice01
+//
+//  Created by yonmo on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
